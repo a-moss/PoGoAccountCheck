68147e0b5f189a5872b227105964cc4bf606f842
Aaron <aaron.moss@mac.com>
2016-08-17 22:07:20 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# PoGoAccountCheck
+PoGo Terms Account Check (Checks if accounts are banned)
+
+# Usage
+
+To use the file use the following command:
+
+python banned.py fileu&p
+
+i.e. python banned.py accounts.txt
+
+If you are experiencing any issues make sure you have ran: pip install -r requirements.txt
+
+# Formatting
+Your file (fileu&p) should be structured as follows:
+
+username1:password1
+
+username2:password2
+
+username3:password3
+
---
+++ banned.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""banned.py - Check multiple PTC accounts ban status with Pokemon Go."""
+
+from pgoapi import PGoApi
+from pgoapi.utilities import f2i
+from pgoapi import utilities as util
+from pgoapi.exceptions import AuthException
+from pgoapi.exceptions import ServerSideRequestThrottlingException
+from pgoapi.exceptions import NotLoggedInException
+import pprint
+import time
+import threading
+import sys, getopt
+import os
+
+def check_account(username, password):
+        api = PGoApi()
+        api.set_position(40.7127837, -74.005941, 0.0)
+        api.login('ptc', username, password)
+        time.sleep(1)
+        req = api.create_request()
+        req.get_inventory()
+        response = req.call()
+        if response['status_code'] == 3:
+            print('The following account is banned! {}'.format(username))
+            if os.path.exists("banned.txt"):
+                f = open('./banned.txt', 'a+b')
+            else:
+                f = open('./banned.txt', 'w+b')
+
+            f.write("%s\n" % (username))
+
+            f.close()
+        else: print('Not banned...')
+
+
+with open(str(sys.argv[1])) as f:
+        credentials = [x.strip().split(':') for x in f.readlines()]
+
+for username,password in credentials:
+        try:
+                check_account(username, password)
+        except ServerSideRequestThrottlingException as e:
+                print('Server side throttling, Waiting 10 seconds.')
+                time.sleep(10)
+                accept_tos(username, password)
+        except NotLoggedInException as e1:
+                print('Could not login, Waiting for 10 seconds')
+                time.sleep(10)
+                accept_tos(username, password)
